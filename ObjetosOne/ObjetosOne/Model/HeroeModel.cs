b4394c62db96bcb97f5529670a2c7f8175178555
Centro de Desarrollo De Software <cds@univo.edu.sv>
2019-10-02 13:52:48 -0600

    rer

---
+++ ObjetosOne/ObjetosOne/Model/HeroeModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjetosOne.Model
+{
+    public class HeroeModel
+    {
+        public string Nombre { get; set; }
+        public string Marca { get; set; }
+        public string Villano { get; set; }
+        public int NumeroMuertes { get; set; }
+
+    }
+}
